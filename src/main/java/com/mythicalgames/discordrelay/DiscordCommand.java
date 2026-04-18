d0b5a0cc1500971aebafee7be83fa73d5bbcdec5
Acktar <admin@acktarcloud.com>
2026-04-18 15:58:15 +0400

    Make command accessible for all

--- src/main/java/com/mythicalgames/discordrelay/DiscordCommand.java
+++ src/main/java/com/mythicalgames/discordrelay/DiscordCommand.java
@@ -3,11 +3,13 @@ package com.mythicalgames.discordrelay;
 import org.allaymc.api.command.SenderType;
 import org.allaymc.api.command.Command;
 import org.allaymc.api.command.tree.CommandTree;
+import org.allaymc.api.permission.OpPermissionCalculator;
 
 public class DiscordCommand extends Command {
 
     public DiscordCommand() {
         super("discord", "Get the Discord server link", "discordrelay.use");
+        OpPermissionCalculator.NON_OP_PERMISSIONS.addAll(this.permissions);
     }
 
     @Override
